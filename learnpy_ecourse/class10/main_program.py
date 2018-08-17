c50f118d01d720592ce6206ebbf09d5d40ff1e5e
zettrider <nico@schemberg.net>
2018-08-17 23:07:27 +0200

    Revert "Merge remote-tracking branch 'refs/remotes/ktbyers/master'"
    This reverts commit f4c0df7af745727628f017c60d06134c7aba1eeb, reversing
    changes made to d8780d79f42474cc43ed82d4fe8331dba9922c56.

--- learnpy_ecourse/class10/main_program.py
+++ learnpy_ecourse/class10/main_program.py
@@ -1,4 +1,5 @@
-"""
+'''
+
 This is a solution to the below problem given the content we have
 discussed in class.  It is not necessarily the best solution to the problem.
 In other words, I generally only use things we have covered up to this point
@@ -85,11 +86,11 @@ Reading objects from pickle files:
          uptime: 8978280
   serial_number: FTX1512038X
 
-"""
+'''
+
 
 import time
 import pickle
-from getpass import getpass
 
 import ssh_connection as ssh
 import telnet_connection as telnet
@@ -97,7 +98,9 @@ from show_version_parser import process_show_version
 
 
 class NetworkDevice(object):
-    """Container for network device attributes."""
+    '''
+    Container for network device attributes
+    '''
     def __init__(self, ip, username, password):
         self.ip = ip
         self.username = username
@@ -106,20 +109,25 @@ class NetworkDevice(object):
 
 
 def ssh_main():
-    """Process show version using SSH."""
-    ip = raw_input("Enter IP Address: ")
-    username = 'pyclass'
-    password = getpass()
+    '''
+    Process show version using SSH
+    '''
+
+    ip = '10.220.88.1'
+    username = 'pynet'
+    password = '*'
 
     test_device = NetworkDevice(ip, username, password)
 
-    (remote_conn_pre, remote_conn, _) = ssh.establish_connection(ip, username, password)
-    ssh.disable_paging(remote_conn)
+    (remote_conn_pre, remote_conn, output) = ssh.establish_connection(ip, username, password)
+    output = ssh.disable_paging(remote_conn)
+
     remote_conn.send("\n")
     remote_conn.send("show version\n")
 
     # Read the output from the 'show version' command
     test_device.show_version = ssh.read_ssh_data(remote_conn)
+
     remote_conn_pre.close()
     process_show_version(test_device)
 
@@ -132,14 +140,18 @@ def ssh_main():
 
 
 def telnet_main():
-    """Process show version using telnet."""
-    ip = raw_input("Enter IP Address: ")
-    username = 'pyclass'
-    password = getpass()
+    '''
+    Process show version using telnet
+    '''
+
+    ip = '10.220.88.1'
+    username = 'pynet'
+    password = '*'
 
     test_device2 = NetworkDevice(ip, username, password)
 
     remote_conn = telnet.establish_connection(ip, username, password)
+
     telnet.disable_paging(remote_conn)
 
     remote_conn.write("\n")
@@ -160,7 +172,10 @@ def telnet_main():
 
 
 def net_device_verification(net_device):
-    """Prints out a set of attributes for a NetworkDevice object."""
+    '''
+    Prints out a set of attributes for a NetworkDevice object
+    '''
+
     print_attr = [
         'hostname',
         'ip',
@@ -178,14 +193,15 @@ def net_device_verification(net_device):
     for field in print_attr:
         val = getattr(net_device, field)
         print "%15s: %-40s" % (field, val)
+
     print
 
 
 def main():
-    """
+    '''
     Login to a network device (using either telnet or SSH) and retrieve 'show version'
     from the device.
-    """
+    '''
     print
     print "Using SSH:"
     ssh_main()
